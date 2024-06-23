591628c5630fd2c0326a5521515143cc09ca48d2
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2024-06-24 00:31:49 +0200

    feat: remove generated file templates from `@nrwl-airlines/nx:angular-library`

--- libs/nx/src/generators/angular-library/generator.ts
+++ libs/nx/src/generators/angular-library/generator.ts
@@ -1,24 +1,10 @@
-import {
-  addProjectConfiguration,
-  formatFiles,
-  generateFiles,
-  Tree,
-} from '@nx/devkit';
-import * as path from 'path';
+import { formatFiles, Tree } from '@nx/devkit';
 import { AngularLibraryGeneratorSchema } from './schema';
 
 export async function angularLibraryGenerator(
   tree: Tree,
-  options: AngularLibraryGeneratorSchema
+  _options: AngularLibraryGeneratorSchema
 ) {
-  const projectRoot = `libs/${options.name}`;
-  addProjectConfiguration(tree, options.name, {
-    root: projectRoot,
-    projectType: 'library',
-    sourceRoot: `${projectRoot}/src`,
-    targets: {},
-  });
-  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
   await formatFiles(tree);
 }
 
