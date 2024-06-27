eaddb4a04465ecc9ccad6e4288091b651ffef846
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2024-06-27 23:10:36 +0200

    feat: generate `@nrwl-airlines/nx:angular-application` generator

---
+++ libs/nx/src/generators/angular-application/generator.spec.ts
@@ -0,0 +1,20 @@
+import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
+import { Tree, readProjectConfiguration } from '@nx/devkit';
+
+import { angularApplicationGenerator } from './generator';
+import { AngularApplicationGeneratorSchema } from './schema';
+
+describe('angular-application generator', () => {
+  let tree: Tree;
+  const options: AngularApplicationGeneratorSchema = { name: 'test' };
+
+  beforeEach(() => {
+    tree = createTreeWithEmptyWorkspace();
+  });
+
+  it('should run successfully', async () => {
+    await angularApplicationGenerator(tree, options);
+    const config = readProjectConfiguration(tree, 'test');
+    expect(config).toBeDefined();
+  });
+});
